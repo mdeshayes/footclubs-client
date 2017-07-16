13c62b89e320ea89a819d68d1a6c11dff1102006
mdeshayes <mathias.deshayes@gmail.com>
2017-07-16 22:41:34 +0200

    extract credentials in a private file

--- src/main/java/fr/asmathieu/licence/LicenceExtractor.java
+++ src/main/java/fr/asmathieu/licence/LicenceExtractor.java
@@ -1,9 +1,15 @@
 package fr.asmathieu.licence;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -29,17 +35,28 @@ public class LicenceExtractor {
 	private static final String BIRTH_DATE_PATTERN = ".*(\\d{2}/\\d{2}/\\d{4}).*";
 	private static final String REGISTER_DATE_PATTERN = ".*selectLicence.*(\\d{2}/\\d{2}/\\d{4}).*";
 	private static final String LICENCE_CATEGORY_PATTERN = ".*profilIndividuReload\\('\\d*','(.{1,40})'\\);\".*";
-	private static final String LICENCE_STATE_PATTERN = ".*setTitle.*>(.*)</a>.*"; //FIXME this pattern doesn't work
+	private static final String LICENCE_STATE_PATTERN = ".*setTitle.*>(.*)</a>.*"; // FIXME
+																					// this
+																					// pattern
+																					// doesn't
+																					// work
 	private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+
+		Properties properties = new Properties();
+		try (InputStream inputStream = LicenceExtractor.class.getResourceAsStream("/credentials.properties")) {
+			properties.load(inputStream);
+		}
+		String licenceQueryParam = properties.getProperty("licence.queryParam");
 
 		Vertx vertx = Vertx.vertx();
 		client = vertx.createHttpClient();
 		LicenceExtractor licenceExtractor = new LicenceExtractor();
 
-		licenceExtractor.login("foo", "bar").flatMap(response -> licenceExtractor.retrieveLicencies(response.headers().get("Set-Cookie"))).flatMap(licenceExtractor::toLicences)
-				.subscribe(System.out::println, System.out::println, () -> {
+		licenceExtractor.login(properties.getProperty("username"), properties.getProperty("password"))
+				.flatMap(response -> licenceExtractor.retrieveLicencies(response.headers().get("Set-Cookie"), licenceQueryParam))
+				.flatMap(licenceExtractor::toLicences).subscribe(System.out::println, System.out::println, () -> {
 					System.out.println("complete");
 					vertx.close();
 				});
@@ -53,7 +70,7 @@ public class LicenceExtractor {
 				Element table = doc.getElementById("EXT_CLB_LIC_LIST_GRID");
 				Element tBody = table.getElementsByTag("tbody").first();
 				Elements rows = tBody.getElementsByTag("tr");
-				rows.stream().filter(element -> {
+				rows.parallelStream().filter(element -> {
 					return !element.className().equals("backmenu");
 				}).map(this::extractLicence).filter(Objects::nonNull).forEach(sub::onNext);
 				sub.onComplete();
@@ -71,7 +88,14 @@ public class LicenceExtractor {
 			LocalDate birthDate = LocalDate.parse(extractLicenceInfo(trElement, BIRTH_DATE_PATTERN), DATE_FORMATTER);
 			LocalDate registerDate = LocalDate.parse(extractLicenceInfo(trElement, REGISTER_DATE_PATTERN), DATE_FORMATTER);
 			LicenceCategory licenceCategory = LicenceCategory.getEnum(extractLicenceInfo(trElement, LICENCE_CATEGORY_PATTERN));
-			LicenceState licenceState = LicenceState.VALIDATED;/*LicenceState.getEnum(extractLicenceInfo(trElement, LICENCE_STATE_PATTERN));*/
+			LicenceState licenceState = LicenceState.VALIDATED;/*
+																 * LicenceState.
+																 * getEnum(
+																 * extractLicenceInfo
+																 * (trElement,
+																 * LICENCE_STATE_PATTERN
+																 * ));
+																 */
 			return new Licence(firstName, lastName, id, birthDate, registerDate, licenceCategory, licenceState);
 		} catch (Exception e) {
 			// TODO
@@ -103,9 +127,9 @@ public class LicenceExtractor {
 		});
 	}
 
-	private Observable<String> retrieveLicencies(String cookie) {
-		return Observable
-				.create(sub -> client.get(new RequestOptions().setPort(443).setHost("footclubs.fff.fr").setURI(LICENCE_PATH).setSsl(true), response -> {
+	private Observable<String> retrieveLicencies(String cookie, String queryParam) {
+		return Observable.create(
+				sub -> client.get(new RequestOptions().setPort(443).setHost("footclubs.fff.fr").setURI(LICENCE_PATH + queryParam).setSsl(true), response -> {
 					response.bodyHandler(buffer -> {
 						String body = new String(buffer.getBytes(), Charset.forName("ISO-8859-15"));
 						sub.onNext(body);
@@ -115,4 +139,3 @@ public class LicenceExtractor {
 	}
 
 }
-
